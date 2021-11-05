cd8b90c051ce55d87a090477858ded1fd63d52ad
alessmorsella <alessandromorsella@gmail.com>
2021-11-04 23:35:35 -0400

    Added TCP Server

--- P2P-FS-Server/main.py
+++ P2P-FS-Server/main.py
@@ -42,6 +42,33 @@ def start_connection():
 
     s.close()
 
+def startTCP():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 10010))
+    server_address = ('localhost', 10000)
+
+    print(sys.stderr, 'starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+
+    sock.listen(1)
+
+    while True:
+        print(sys.stderr, 'Waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print(sys.stderr, 'connection from', client_address)
+            while True:
+                data = connection.recv(16)
+                print(sys.stderr, 'recieved "%s"' % data)
+                if data:
+                    print(sys.stderr, 'sending data back to the client')
+                    connection.sendall(data)
+                else:
+                    print(sys.stderr, 'no more data from', client_address)
+                    break
+        finally:
+            connection.close()
 
 if __name__ == '__main__':
     start_connection()
+    startTCP()
