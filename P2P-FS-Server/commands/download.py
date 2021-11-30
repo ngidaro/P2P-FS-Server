82327c235205108a59ec9715a1d028bea3efe3c7
Nicholas <ngidaro@hotmail.com>
2021-11-30 11:43:21 -0500

    Download file from a user's folder

--- P2P-FS-Server/commands/download.py
+++ P2P-FS-Server/commands/download.py
@@ -3,18 +3,25 @@ import os
 
 
 def download(parsed_msg, connection):
-    downloadFile = parsed_msg[2]
-    downloadFilePath = os.path.abspath(downloadFile)
+    fileInfo = parsed_msg[2].split('/')
+
+    # fileInfo[0]: Client to download from
+    # fileInfo[1]: File to download
+
+    if len(fileInfo) != 2:
+        return f"DOWNLOAD-ERROR {parsed_msg[1]} FILE NOT FOUND"
+
+    downloadFilePath = f"public/{fileInfo[0]}/{fileInfo[1]}"
 
     if os.path.exists(downloadFilePath):
-        serializedDownloadFile = serializeFiles(downloadFilePath)
+        serializedDownloadFile = serializeFile(downloadFilePath)
         connection.sendall(serializedDownloadFile)
         return 'FILE SENT SUCCESSFULLY:' + parsed_msg[1]  # Add filename, chunk text
     else:
         return f"DOWNLOAD-ERROR {parsed_msg[1]} FILE NOT FOUND"
 
 
-def serializeFiles(filename):
+def serializeFile(filename):
     allFileContent = []
     file = open(filename, 'r')
     content = file.readlines()
